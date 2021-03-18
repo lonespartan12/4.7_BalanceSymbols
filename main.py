aaff1d6e0a1df79a8042c43ee159e6ec3adfc894
Christian Mudd <lonespartan12@hotmail.com>
2021-03-18 06:45:15 +0000

    testing things

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 from pythonds.basic import Stack
 
+
 def parChecker(symbolString):
-  s = Stack()
-  balanced = True
-  index = 0
-  while index < len(symbolString) and balanced:
-    symbol = symbolString[index]
-    if symbol in "([{":
-      s.push(symbol)
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol in "([{":
+            s.push(symbol)
+            print(f"the symbol {symbol}\nat index {index} \nhas been added to the stack\n {s.__str__} \n")
+        else:
+            if s.isEmpty():
+                balanced = False
+                print("line 16 stack is empty")
+            else:
+                top = s.pop()
+                print(f"the symbol {top} \nhas been removed from the stack")
+                #print(s)
+                print("\n")
+                if not matches(top, symbol):
+                    balanced = False
+                    print("line 22 stack is not balanced")
+        index = index + 1
+    if balanced and s.isEmpty():
+        return True
     else:
-      if s.isEmpty():
-        balanced = False
-      else:
-        top = s.pop()
-        if not matches(top,symbol):
-          balanced = False
-    index = index + 1
-  if balanced and s.isEmpty():
-    return True
-  else:
-    return False
+        return False
 
 def matches(open, close):
-  opens = "([{"
-  closers = ")]}"
-  return opens.index(open) == closers.index(close)
-        
+    opens = "([{"
+    closers = ")]}"
+    return opens.index(open) == closers.index(close)
 
 
 print(parChecker('{({([][])}())}'))
